44e85b22b681936ebb0a102fea6988495e6b2756
Caesar Kabalan <caesar.kabalan@gmail.com>
2018-01-08 23:43:35 -0700

    Implement basic API Gateway Lambda Proxy for Ratings

--- handler.py
+++ handler.py
@@ -1,24 +1,35 @@
+import boto3
+import zlib
 import json
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('GraphTV-Ratings-Prod')
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
+def get_ratings(event, context):
+    print(event)
+    response = table.get_item(
+        Key={
+            'id': event['pathParameters']['showId']
+        }
+    )
+    # Return Base64-encoded ZLIB binary data (1x execution time)
+    # return {'ratings': base64.b64encode(response['Item']['data'].value).decode('utf-8')}
+    #
+    # Return decompressed JSON data (415x execution time)
+    # return json.loads(zlib.decompress(response['Item']['data'].value).decode())
+    #return {'data': base64.b64encode(response['Item']['data'].value).decode('utf-8')}
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        'statusCode': 200,
+        'headers': {
+            'x-custom-header': 'my custom header value'
+        },
+        'body': zlib.decompress(response['Item']['data'].value).decode(),
+        'isBase64Encoded': False
     }
-    """
+
+
+if __name__ == '__main__':
+    # execute only if run as the entry point into the program
+    get_ratings({"id": "tt0903747"}, None)
+    #get_ratings({"id": "tt0058796"}, None)
